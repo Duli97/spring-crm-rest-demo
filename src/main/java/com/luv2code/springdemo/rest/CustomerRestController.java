f54a89517b403d5b55d056513b230bbec3053e94
Duli97 <dwindula@gmail.com>
2020-12-08 01:08:06 +0530

    delete customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,9 +3,11 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +51,37 @@ public class CustomerRestController {
 		customerService.saveCustomer(theCustomer);
 		
 		return theCustomer;
+	}
+	
+	//add maping for PUT / customers - update existing customers
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
+		//also just in case the pass an id in JSON ... set id to 0		
+		//this is force a save of new item ... instead of update
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	//add maping for DELETE /customers/{customerId} - delete existing customers
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		//also just in case the pass an id in JSON ... set id to 0		
+		//this is force a save of new item ... instead of update
+		
+		Customer tempCustomer = customerService.getCustomer(customerId);
+		
+		//throw exception if null
+		if(tempCustomer == null) {
+			throw new CustomerNotFoundException("Customer is not found - " + customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
+		
+		return "delete customer id - " + customerId;
 	}
 	
 
