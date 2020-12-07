52f12040b722e4456bbb7d626fe21e568665d105
Duli97 <dwindula@gmail.com>
2020-12-07 22:32:46 +0530

    no message

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class CustomerRestController {
 	private CustomerService customerService;
 	
 	//add mapping for GET / customers
+	@GetMapping("/customers")
 	public List<Customer> getCustomers(){
 		return customerService.getCustomers();
 	}
